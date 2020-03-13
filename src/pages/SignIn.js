476de708d91289183877138cfba9041b9af01858
florianGierlichs <gierlichs@gmx.de>
2020-03-13 16:55:13 +0100

    Add input style

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -23,55 +23,56 @@ function SignIn() {
       <h1 className="main__h1">Sign In</h1>
       <form className="signin-form__container" onSubmit={handleSubmit}>
         <input type="file" className="inputfile inputfile-4" />
-        <label>
+        <label className="label-image">
           <figure>
             <img src={SignInImage} alt="Sign-In logo"></img>
           </figure>
 
           <span>Lade ein Bild von Dir hoch!</span>
         </label>
-        <label className="signin-form__container__input-label">
-          <Input
-            className="input"
-            placeholder="Nickname"
-            value={nickname}
-            onChange={event => {
-              setNickname(event.target.value);
-            }}
-          />
-        </label>
-        <label className="signin-form__container__input-label">
-          <Input
-            type="number"
-            className="input"
-            placeholder="Wähle dein Geburtsjahr"
-            value={yearOfBirth}
-            onChange={event => {
-              setYearOfBirth(event.target.value);
-            }}
-          />
-        </label>
-        <label className="signin-form__container__input-label">
-          <Input
-            className="input"
-            placeholder="Wähle dein Geschlecht"
-            value={sex}
-            onChange={event => {
-              setSex(event.target.value);
-            }}
-          />
-        </label>
-        <label className="signin-form__container__input-label">
-          <Input
-            className="input"
-            placeholder="Wie lang ist deine Angel?"
-            value={distance}
-            onChange={event => {
-              setDistance(event.target.value);
-            }}
-          />
-        </label>
-        <div>inputs</div>
+        <div className="signin-form__container__input-container">
+          <label className="signin-form__container__input-label">
+            <Input
+              className="input"
+              placeholder="Nickname"
+              value={nickname}
+              onChange={event => {
+                setNickname(event.target.value);
+              }}
+            />
+          </label>
+          <label className="signin-form__container__input-label">
+            <Input
+              type="number"
+              className="input"
+              placeholder="Wähle dein Geburtsjahr"
+              value={yearOfBirth}
+              onChange={event => {
+                setYearOfBirth(event.target.value);
+              }}
+            />
+          </label>
+          <label className="signin-form__container__input-label">
+            <Input
+              className="input"
+              placeholder="Wähle dein Geschlecht"
+              value={sex}
+              onChange={event => {
+                setSex(event.target.value);
+              }}
+            />
+          </label>
+          <label className="signin-form__container__input-label">
+            <Input
+              className="input"
+              placeholder="Wie lang ist deine Angel?"
+              value={distance}
+              onChange={event => {
+                setDistance(event.target.value);
+              }}
+            />
+          </label>
+        </div>
         <button></button>
       </form>
     </>
