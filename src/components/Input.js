a5c3365903ae5dab6c10cf3a4c434605ddfe2c83
florianGierlichs <gierlichs@gmx.de>
2020-03-13 16:10:31 +0100

    Add input fields

---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Input.css';
+
+function Input(props) {
+  return <input {...props} />;
+}
+
+export default Input;
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,26 +1,76 @@
 import React from 'react';
 import './SignIn.css';
 import SignInImage from '../assets/signin.svg';
+import Input from '../components/Input';
 
 function SignIn() {
+  const [nickname, setNickname] = React.useState('');
+  const [yearOfBirth, setYearOfBirth] = React.useState('');
+  const [sex, setSex] = React.useState('');
+  const [distance, setDistance] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(nickname);
+    console.log(yearOfBirth);
+    console.log(sex);
+    console.log(distance);
+  }
+
   return (
     <>
       <h1 className="main__h1">Sign In</h1>
-      <form className="signin-form__container">
-        <input
-          type="file"
-          name="file-5"
-          id="file-5"
-          class="inputfile inputfile-4"
-        />
-        <label for="file-5">
+      <form className="signin-form__container" onSubmit={handleSubmit}>
+        <input type="file" className="inputfile inputfile-4" />
+        <label>
           <figure>
             <img src={SignInImage} alt="Sign-In logo"></img>
           </figure>
 
           <span>Lade ein Bild von Dir hoch!</span>
         </label>
-
+        <label className="signin-form__container__input-label">
+          <Input
+            className="input"
+            placeholder="Nickname"
+            value={nickname}
+            onChange={event => {
+              setNickname(event.target.value);
+            }}
+          />
+        </label>
+        <label className="signin-form__container__input-label">
+          <Input
+            type="number"
+            className="input"
+            placeholder="Wähle dein Geburtsjahr"
+            value={yearOfBirth}
+            onChange={event => {
+              setYearOfBirth(event.target.value);
+            }}
+          />
+        </label>
+        <label className="signin-form__container__input-label">
+          <Input
+            className="input"
+            placeholder="Wähle dein Geschlecht"
+            value={sex}
+            onChange={event => {
+              setSex(event.target.value);
+            }}
+          />
+        </label>
+        <label className="signin-form__container__input-label">
+          <Input
+            className="input"
+            placeholder="Wie lang ist deine Angel?"
+            value={distance}
+            onChange={event => {
+              setDistance(event.target.value);
+            }}
+          />
+        </label>
         <div>inputs</div>
         <button></button>
       </form>
