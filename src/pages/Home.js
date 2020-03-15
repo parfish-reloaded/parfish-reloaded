c629a1890702d7a6dca7579ba9b3bdfd9173468a
Uwe Steinberg <uwe.steinberg@tristero.de>
2020-03-15 10:17:09 +0100

    Add form and buttons to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import './Home.css';
+import Input from '../components/Input';
+import Button from '../components/Button';
 
-function Home() {
-  return <></>;
+function Home(props) {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  function handleSubmit() {}
+  return (
+    <>
+      <h1 className="main__h1 main__h1__b">
+        Jeder Fisch braucht seinen Schwarm
+      </h1>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <Input
+          className="register-form__email"
+          type="email"
+          required
+          value={email}
+          placeholder="Email"
+          onChange={event => {
+            setEmail(event.target.value);
+          }}
+        />
+        <Input
+          className="register-form__password"
+          type="password"
+          required
+          value={password}
+          placeholder="Passwort"
+          onChange={event => {
+            setPassword(event.target.value);
+          }}
+        />
+        <Button className="register-form__button">Let's Go Fishing!</Button>
+      </form>
+    </>
+  );
 }
 export default Home;
